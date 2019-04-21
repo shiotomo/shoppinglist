bee613d526fd22aaf802f8d1847b1ca12d53be9b
shiotomo <t.shiota16@gmail.com>
2019-04-21 18:07:56 +0900

    Productを登録するためのAPIを実装した #2

--- web/src/main/java/list/shopping/web/controller/api/ApiProductController.java
+++ web/src/main/java/list/shopping/web/controller/api/ApiProductController.java
@@ -1,9 +1,29 @@
 package list.shopping.web.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import list.shopping.web.entity.Product;
+import list.shopping.web.service.ProductService;
+import lombok.val;
 
 @RequestMapping("/api/product")
 @RestController
 public class ApiProductController {
+    @Autowired
+    private ProductService productService;
 
+    /**
+     * Productを登録する
+     *
+     * @param product
+     * @return
+     */
+    @PostMapping("/create")
+    public String recordProduct(@RequestBody @Validated Product product) {
+        productService.newSave(product);
+        val status = "{status: \"200\"}";
+        return status;
+    }
 }
