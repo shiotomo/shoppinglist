a4bbc6da718382ed3ba932d0b609e3cc8c4553b9
shiotomo <t.shiota16@gmail.com>
2019-04-22 03:21:02 +0900

    modalを更新した

--- bot/model/product.py
+++ bot/model/product.py
@@ -1,13 +1,37 @@
-from datetime import *
-import sqlalchemy
-import sqlalchemy.ext.declarative
+class Product():
+    def __init__(number, name, status, created_at, updated_at):
+        self.number = number
+        self.name = name
+        self.status = status
+        self.created_at = created_at
+        self.updated_at = updated_at
 
-Base = sqlalchemy.ext.declarative.declarative_base()
+    def get_id():
+        return self.number
 
-class Product(Base):
-    __tablename__ = 'product'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    name = sqlalchemy.Column(sqlalchemy.String(256))
-    status = sqlalchemy.Column(sqlalchemy.String(256))
-    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now(JST()))
-    updated_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now(JST()))
+    def get_name():
+        return self.name
+
+    def get_status():
+        return self.status
+
+    def get_created_at():
+        return self.created_at
+
+    def get_updated_at():
+        return self.updated_at
+
+    def set_id(number):
+        self.number = number
+
+    def set_name(name):
+        self.name = name
+
+    def set_status(status):
+        self.status = status
+
+    def set_created_at(created_at):
+        self.created_at = created_at
+
+    def set_updated_at(updated_at):
+        self.updated_at = updated_at
