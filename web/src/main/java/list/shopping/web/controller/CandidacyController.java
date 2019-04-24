76156eaa8503944a46a5c3726f53a079466c200c
shiotomo <t.shiota16@gmail.com>
2019-04-24 23:39:17 +0900

    候補を登録できるようにした

--- web/src/main/java/list/shopping/web/controller/CandidacyController.java
+++ web/src/main/java/list/shopping/web/controller/CandidacyController.java
@@ -1,19 +1,27 @@
 package list.shopping.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import list.shopping.web.entity.Candidacy;
+import list.shopping.web.service.CandidacyService;
 
-@RequestMapping("/candidacy")
+@RequestMapping("/product/{productId}/candidacy")
+@Controller
 public class CandidacyController {
+
+    @Autowired
+    private CandidacyService candidacyService;
 
     /**
      * 商品に紐付いた候補を登録する
      *
      * @param productId
      */
-    @PostMapping("/create/product/{productId}")
+    @PostMapping("/create")
     public String create(@PathVariable int productId, Candidacy candidacy) {
+        candidacyService.insert(productId, candidacy);
         return "redirect:/dashboard/" + productId;
     }
 
@@ -24,9 +32,8 @@ public class CandidacyController {
      * @param id
      * @return
      */
-    @DeleteMapping("/delete/{id}/product/{productId}")
+    @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable int id, @PathVariable int productId) {
         return "redirect:/dashboard/" + productId;
     }
-
 }
